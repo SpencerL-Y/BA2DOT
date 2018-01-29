5c80847ab7d4141d1ba0e967d55a7dbd2415b068
Clexma <lixie15@mails.ucas.ac.cn>
2018-01-29 22:45:11 +0800

    Mid to dot

--- src/main/MidToDOT.java
+++ src/main/MidToDOT.java
@@ -1,5 +1,35 @@
 package main;
 
-public class MidToDOT {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class MidToDOT {
+	public static void midToDOT(MidComponent mid, String path) throws IOException {
+		File f = new File(path);
+		FileWriter fw = new FileWriter(f);
+		String tempLine = "diagraph graph {";
+		fw.write(tempLine);
+		Integer initNum = mid.getInit().getId();
+		tempLine = "size = 5";
+		fw.write(tempLine);
+		tempLine = "node [shape = point] INIT;";
+		fw.write(tempLine);
+		tempLine = "node [shape = double circle]";
+		for(State s : mid.accStates) {
+			tempLine += " S" + s.getId() + " ";
+		}
+		tempLine += ";";
+		fw.write(tempLine);
+		tempLine = "node [shape = circle];";
+		fw.write(tempLine);
+		tempLine = "INIT -> S" + mid.getInit().getId() + ";";
+		for(Transition t : mid.transitions) {
+			tempLine = "S" + t.getFrom() + " -> " + "S" + t.getTo() + "[label =\"" + t.getLetter() + "\" ];";
+			fw.write(tempLine);
+		}
+		tempLine = "}";
+		fw.write(tempLine);
+		fw.close();
+	}
 }
