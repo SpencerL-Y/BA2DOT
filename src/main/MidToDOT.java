f640b519fc612e7516fd739b11943995812ae79c
Clexma <lixie15@mails.ucas.ac.cn>
2018-01-29 23:11:39 +0800

    DOT syntax error

--- src/main/MidToDOT.java
+++ src/main/MidToDOT.java
@@ -8,26 +8,27 @@ public class MidToDOT {
 	public static void midToDOT(MidComponent mid, String path) throws IOException {
 		File f = new File(path);
 		FileWriter fw = new FileWriter(f);
-		String tempLine = "diagraph graph {";
+		String tempLine = "digraph result {\n";
 		fw.write(tempLine);
-		tempLine = "size = 5";
+		tempLine = "size = \"2\";\n";
 		fw.write(tempLine);
-		tempLine = "node [shape = point] INIT;";
+		tempLine = "node [shape = point] INIT;\n";
 		fw.write(tempLine);
-		tempLine = "node [shape = double circle]";
+		tempLine = "node [shape = doublecircle]";
 		for(State s : mid.accStates) {
 			tempLine += " S" + s.getId() + " ";
 		}
-		tempLine += ";";
+		tempLine += ";\n";
 		fw.write(tempLine);
-		tempLine = "node [shape = circle];";
+		tempLine = "node [shape = circle];\n";
 		fw.write(tempLine);
-		tempLine = "INIT -> S" + mid.getInit().getId() + ";";
+		tempLine = "INIT -> S" + mid.getInit().getId() + ";\n";
+		fw.write(tempLine);
 		for(Transition t : mid.transitions) {
-			tempLine = "S" + t.getFrom() + " -> " + "S" + t.getTo() + "[label =\"" + t.getLetter() + "\" ];";
+			tempLine = "S" + t.getFrom() + " -> " + "S" + t.getTo() + "[label =\"" + t.getLetter() + "\" ];\n";
 			fw.write(tempLine);
 		}
-		tempLine = "}";
+		tempLine = "}\n";
 		fw.write(tempLine);
 		fw.close();
 	}
